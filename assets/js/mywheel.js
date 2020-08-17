7243a589a1c83d36cb81ccfe2595751298dcfa17
Your Name <you@example.com>
2020-08-17 13:02:00 -0400

    debug

--- assets/js/mywheel.js
+++ assets/js/mywheel.js
@@ -2,7 +2,7 @@
             console.log("POOP 3");
             let theWheel = new Winwheel({
                 'numSegments'       : 16,                 // Specify number of segments.
-                'outerRadius'       : 200,               // Set outer radius so wheel fits inside the background.
+                'outerRadius'       : 400,               // Set outer radius so wheel fits inside the background.
                 'drawText'          : true,              // Code drawn text can be used with segment images.
                 'textFontSize'      : 16,
                 'textOrientation'   : 'curved',
