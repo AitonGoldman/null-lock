77ee9ff8fdb2efae502265933ddd95e7bc6d19d5
Your Name <you@example.com>
2020-08-31 19:22:25 -0400

    whatever

--- js/spock.2.js
+++ js/spock.2.js
@@ -4,7 +4,7 @@ function loadImages(){
         var i = document.createElement('img');
         i.src="https://aitongoldman.github.io/null-lock/img/"+spockPics[imgId];
         i.id=imgId;
-        i.style="visibility:hidden;position:absolute;";
+        //i.style="visibility:hidden;position:absolute;";
         document.body.appendChild(i);
     }    
 }
